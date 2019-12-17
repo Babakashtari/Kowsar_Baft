f5171e7632ae296823093dbce0f53c584dad936c
babakashtari <ashtaribabak@rocketmail.com>
2019-12-18 02:30:29 +0330

    added kalite cells

--- JS/kaliteDotted.js
+++ JS/kaliteDotted.js
@@ -1,6 +1,38 @@
-const kaliteContainer = document.querySelector("main > section:first-of-type#kalite_container > div#cell_container");
-const kaliteImage = document.querySelector('main>section:first-of-type#kalite_container> div#cell_container>img');
+const kaliteContainer = document.querySelector(
+    "main > section:first-of-type#kalite_container > div#cell_container"
+);
+const kaliteImage = document.querySelector(
+    "main>section:first-of-type#kalite_container> div#cell_container>img"
+);
+// background gradients:
+const kalite_cells_gradients = [
+    "radial-gradient(circle, rgb(50, 36, 27) 10px, transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.8) 10px,transparent 10px)",
+    "radial-gradient(circle,rgb(199, 123, 101) 10px,transparent 10px)",
+    "radial-gradient(circle,rgb(199, 123, 101) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.5) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.5) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.5) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.6) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.5) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.7) 10px,transparent 10px)",
+    "radial-gradient(circle,rgba(255, 255, 255, 0.4) 10px,transparent 10px)"
+];
+// background colors:
+const kalite_cells_background_colors = [
+    "rgb(205, 218, 96)",
+    "rgb(210, 77, 76)",
+    "rgb(84, 0, 0)",
+    "rgb(34, 0, 0)",
+    "rgb(128, 0, 128)",
+    "rgb(0, 0, 40)",
+    "rgb(35, 0, 44)",
+    "rgb(2, 19, 13)",
+    "rgb(74, 59, 16)",
+    "rgb(48, 64, 47)",
+    "rgb(4, 12, 3)"
+];
 
-kaliteContainer.addEventListener('click', (e)=>{
+kaliteContainer.addEventListener("click", e => {
     console.log(e.target);
-})
+});
